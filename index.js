14a9b3f7afd54d7cb9f0fbf9b3fd16ed555f14eb
Emmanue <kioxmanu7@gmail.com>
2023-10-03 12:14:00 +0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var textParagraph = document.getElementById("text");
+  
+    if (textParagraph) {
+      textParagraph.textContent = "This is really cool!";
+    }
+  });
+  
